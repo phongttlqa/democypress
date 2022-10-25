6e90562675267507d6de8d7a153e10a94043f871
USER\phongtt5 <phongtt5@tcbs.com.vn>
2022-10-25 14:43:28 +0700

    create project demo cypress

---
+++ cypress/integration/BE_play_EL.js
@@ -0,0 +1,11 @@
+describe('Example to demonstrate API Mocking in Cypress with TCi3', () => {
+
+    it('Mock margin data',() => {
+            
+            //Dùng để điều chỉnh độ phân giải của màn hình test
+            cy.viewport(1900, 969)
+            //visit dùng để điều hướng tới url mong muốn
+            cy.visit('https://www.google.com/')
+          })
+        
+  })
